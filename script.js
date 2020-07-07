5c61c3690fe7cd866f3482b04ee2d48d623bccc9
nbankes <nbhankes@gmail.com>
2020-07-06 17:02:00 -0700

    Build Out of Site Layout

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 gsap.from(".socialMediaContainer", { duration: 1, y: "-100" });
+
+function portfolioLink() {
+  var portfolio = document.getElementById("portfolio");
+
+  portfolio.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
+
+function resumeLink() {
+  var resume = document.getElementById("resume");
+
+  resume.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
+
+function contactLink() {
+  var contact = document.getElementById("contact");
+
+  contact.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
