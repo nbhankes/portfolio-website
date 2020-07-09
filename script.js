2d54f8096dc874907a8d5bb46b886c847de094a6
nbankes <nbhankes@gmail.com>
2020-07-08 21:48:06 -0700

    Worked on styling portfolio section

--- script.js
+++ script.js
@@ -34,6 +34,30 @@ function contactLink() {
   });
 }
 
+//! **************** PORTFOLIO BEHAVIOR ****************
+
+function portfolioCohort() {
+  event.preventDefault();
+  Swal.fire({
+    title: "This project was all about teamwork, work flow, and communication!",
+    html: `
+    <ul style="text-align:left">
+      <li>As team leader, I set project scope and provided design guidance while relying on agile methodology, 
+      GitHub workflows, and remote communication to deliver this product to spec and on time. </li>
+      </br> </br> 
+      <li>My contribution to the project design was the team member introduction cards and the iframe embed.</li>
+      </br> </br>
+      <li>This project is built in HTML and CSS and was deployed to the web via Surge.
+      Our team utilized pull requests, issue tickets, Zoom, and Slack messenger to work together as a team.</li>
+    </ul>
+      `,
+    backdrop: `
+    rgba(0,0,0,0.7)
+    `,
+    showConfirmButton: false,
+  });
+}
+
 //! **************** FORM BEHAVIOR ****************
 
 function submissionAlert() {
@@ -43,5 +67,8 @@ function submissionAlert() {
     title: "Thank you!",
     text: "You'll hear back shortly.",
     showConfirmButton: false,
+    backdrop: `
+    rgba(0,0,0,0.7)
+    `,
   });
 }
